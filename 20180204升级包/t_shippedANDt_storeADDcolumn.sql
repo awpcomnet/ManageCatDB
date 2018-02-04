ca945e9934b876ee676312f784ebfa2543a5d8e2
wh <awpcomnet@163.com>
2018-02-04 19:50:04 +0800

    提交升级脚本

--- 20180204升级包/t_shippedANDt_storeADDcolumn.sql
+++ 20180204升级包/t_shippedANDt_storeADDcolumn.sql
@@ -1,4 +1,5 @@
 use catdb;
+
 alter table t_shipped add date_of_manufacture varchar(8);
 alter table t_shipped add quality_guarantee_period DOUBLE;
 alter table t_shipped add period_of_validity varchar(8);
